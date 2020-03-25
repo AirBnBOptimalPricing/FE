2bbefe4404e2b729129dc8f9de8defa061cd3f68
victor tran <victor.tran@tempestas.io>
2020-03-25 02:17:08 -0400

    Change layout of input so for different cases (i.e checkbox)

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import { Field } from 'formik';
 import { capitalize } from '../../util';
 
-const Input = ({ id, ...props }) => {
+const Input = ({ id, altText, className = '', ...props }) => {
+    const isCheckBox = props.type === 'checkbox';
     return (
-        <div className="form-input">
-            <label htmlFor={id}>{capitalize(props.name)}:</label>
-            <Field {...props} id={id} />
+        <div className={`form-input ${className}`.trim()}>
+            {!isCheckBox ? (
+                <>
+                    <label htmlFor={id}>
+                        {altText || capitalize(props.name)}:
+                    </label>
+                    <Field {...props} id={id} />
+                </>
+            ) : (
+                <>
+                    <Field {...props} id={id} />
+                    <label htmlFor={id}>
+                        {altText || capitalize(props.name)}
+                    </label>
+                </>
+            )}
         </div>
     );
 };

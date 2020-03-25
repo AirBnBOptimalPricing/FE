6eafc0bb15c8d12e84f6b80298899fc2ce8f2714
victor tran <victor.tran@tempestas.io>
2020-03-25 02:56:07 -0400

    add a comment for future reference

--- src/components/Route/PrivateRoute.jsx
+++ src/components/Route/PrivateRoute.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useToken } from '../../hooks';
 
-const PrivateRoute = ({ component: Component, props }) => {
+const PrivateRoute = ({ component: Component, ...props }) => {
     const token = useToken();
+    // Needs a safeguard, such as, when a user goes to edit a property, they must OWN that property
     return (
         <Route
             {...props}

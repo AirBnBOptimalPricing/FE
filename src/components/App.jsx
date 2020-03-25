9889efaef5c8a875027c32af9f383541c2d129ce
victor tran <victor.tran@tempestas.io>
2020-03-24 23:29:02 -0400

    Add routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { AddListing, PrivateRoute } from './';
 
 function App() {
-  return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Switch>
+                <Route path="/login" render={() => <div>Login</div>} />
+                <Route path="/register" render={() => <div>Register</div>} />
+                {/* will have a separate router underneath of it */}
+                <PrivateRoute
+                    path="/property/new"
+                    exact
+                    component={AddListing}
+                />
+                <Route
+                    path="/property"
+                    component={() => (
+                        <div>
+                            {/* will have a separate router inside of it */}
+                            Properties
+                        </div>
+                    )}
+                />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;

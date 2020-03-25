ed9feba950c48616f7d7f83b39b597dad1226960
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-03-24 20:09:00 -0700

    Worling on login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Login } from '.';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+     <Login/>
+  </div>;
 }
 
 export default App;

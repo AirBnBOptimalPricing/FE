e415efb08dac863731865c7727a4c32d0f388b3f
victor tran <victor.tran@tempestas.io>
2020-03-24 22:31:09 -0400

    added more components to master export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 import App from './App';
+import AddListing from './Form/AddListing';
+import DeleteListingButton from './Form/DeleteListingButton';
+import Login from './Form/Login';
+import Register from './Form/Register';
+import UpdateListing from './Form/UpdateListing';
 
-export { App };
+export { App, AddListing, DeleteListingButton, Login, Register, UpdateListing };

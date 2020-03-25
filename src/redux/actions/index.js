7f2e7a262fcc5090e37dec22b9c485ed67656838
victor tran <victor.tran@tempestas.io>
2020-03-25 03:55:03 -0400

    create some actions types

---
+++ src/redux/actions/index.js
@@ -0,0 +1,5 @@
+export const ASYNC_START = 'ASYNC_START';
+export const ASYNC_SUCCESS = 'ASYNC_SUCCESS';
+export const ASYNC_FAILURE = 'ASYNC_FAILURE';
+
+export const TOGGLE_MODAL = 'TOGGLE_MODAL';

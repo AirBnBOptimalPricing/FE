af51f20258a1a5792293a6cfc0ccca60806f5bf8
victor tran <victor.tran@tempestas.io>
2020-03-25 20:27:38 -0400

    combined reducers

---
+++ src/redux/reducers/modalReducer.js
@@ -0,0 +1,8 @@
+import { initialState } from '../initialState';
+
+export const modal = (status = { ...initialState.modal }, action) => {
+    switch (action.type) {
+        default:
+            return { ...state };
+    }
+};

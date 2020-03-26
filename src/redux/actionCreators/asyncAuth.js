468457b5a344d350c4b6cc79119e6357025011f0
victor tran <victor.tran@tempestas.io>
2020-03-25 20:43:06 -0400

    add use of dispatch

--- src/redux/actionCreators/asyncAuth.js
+++ src/redux/actionCreators/asyncAuth.js
@@ -2,25 +2,25 @@ import { authStart, authSuccess, authFailure } from './';
 import { withAuth } from '../../util';
 
 export const login = credentials => async dispatch => {
-    authStart();
+    dispatch(authStart());
     try {
         const response = await withAuth('/auth/login/', 'post', credentials);
         const payload = { token: response.data.token };
-        authSuccess(payload);
+        dispatch(authSuccess(payload));
     } catch (error) {
         console.log(error);
-        authFailure(error);
+        dispatch(authFailure(error));
     }
 };
 
 export const register = credentials => async dispatch => {
-    authStart();
+    dispatch(authStart());
     try {
         const response = await withAuth('/auth/register/', 'post', credentials);
         const payload = { token: '' };
-        authSuccess(payload);
+        dispatch(authSuccess(payload));
     } catch (error) {
         console.log(error);
-        authFailure(error);
+        dispatch(authFailure(error));
     }
 };

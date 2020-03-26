7ebe3ed749136017ab9409b0e9f007e72112be04
victor tran <victor.tran@tempestas.io>
2020-03-25 22:35:38 -0400

    add the action creators for adding property to export list

--- src/redux/actionCreators/index.js
+++ src/redux/actionCreators/index.js
@@ -1,4 +1,16 @@
 import { authStart, authFailure, authSuccess } from './auth';
 import { login, register } from './asyncAuth';
+import { addFail, addStart, addSuccess } from './addProperty';
+import { addProperty } from './asyncAdd';
 
-export { authStart, authFailure, authSuccess, login, register };
+export {
+    authStart,
+    authFailure,
+    authSuccess,
+    login,
+    register,
+    addFail,
+    addStart,
+    addSuccess,
+    addProperty,
+};

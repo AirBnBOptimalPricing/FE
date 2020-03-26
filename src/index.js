5188ff33867f561890125f28250821a48c4c9e41
victor tran <victor.tran@tempestas.io>
2020-03-25 20:36:41 -0400

    hook up store to app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './sass/index.scss';
 import { App } from './components';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <React.StrictMode>
+        <Router>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change

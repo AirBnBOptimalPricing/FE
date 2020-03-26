970ab90d248d2d01af5134bab31b38efbf726f1f
victor tran <victor.tran@tempestas.io>
2020-03-26 04:31:33 -0400

    export getProperties

--- src/redux/actionCreators/index.js
+++ src/redux/actionCreators/index.js
@@ -3,6 +3,7 @@ import { login, register } from './asyncAuth';
 import { addFail, addStart, addSuccess } from './addProperty';
 import { addProperty } from './asyncAdd';
 import { setLoggedInUser } from './setLoggedInUser';
+import { getProperties } from './getProperties';
 
 export {
     authStart,
@@ -15,4 +16,5 @@ export {
     addSuccess,
     addProperty,
     setLoggedInUser,
+    getProperties,
 };

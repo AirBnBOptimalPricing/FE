34a9585e1fa1c35c9efcfed99dd5fd3915b0cad2
victor tran <victor.tran@tempestas.io>
2020-03-25 21:55:26 -0400

    remove extra call to redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,9 +6,5 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
     rootReducer,
-    composeEnhancers(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
+    composeEnhancers(applyMiddleware(thunk)),
 );

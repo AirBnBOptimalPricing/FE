ab794f469b529f3ca8a83c34fa9b9e6ead510a86
victor tran <victor.tran@tempestas.io>
2020-03-25 20:30:39 -0400

    combined reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { auth } from './authReducer';
+import { modal } from './modalReducer';
+import { property } from './propertyReducer';
 
-const rootReducer = combineReducers({ auth });
+const rootReducer = combineReducers({ auth, modal, property });
 
 export default rootReducer;

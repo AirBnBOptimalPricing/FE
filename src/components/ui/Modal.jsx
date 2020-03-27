8736e55565c8501d3b4760f027a16c2897f716ea
victor tran <victor.tran@tempestas.io>
2020-03-27 02:52:08 -0400

    move components in ui folder

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Backdrop } from './';
-const Modal = ({ className = '' }) => {
+import { Backdrop } from '..';
+const Modal = ({ className = '', children }) => {
     return (
         <div className={`${className} modal`.trim()}>
             <Backdrop />
+            {children}
         </div>
     );
 };

82513393dd7a48c1414fa61e479df222e4a8eec2
victor tran <victor.tran@tempestas.io>
2020-03-27 04:12:28 -0400

    write modal reducer and an action creator

--- src/redux/reducers/modalReducer.js
+++ src/redux/reducers/modalReducer.js
@@ -1,11 +1,28 @@
+import { TOGGLE_MODAL } from '../actions';
+
 const initialState = {
     sideDrawer: {
         show: false,
     },
 };
 
+const toggleModal = modalName => {
+    return {
+        type: TOGGLE_MODAL,
+        payload: modalName,
+    };
+};
+
 export const modal = (state = initialState, action) => {
     switch (action.type) {
+        case TOGGLE_MODAL:
+            return {
+                ...state,
+                [action.payload]: {
+                    ...state[action.payload],
+                    show: !state[action.payload].show,
+                },
+            };
         default:
             return { ...state };
     }

8e39bdca3cc5645d99e5c0634c0f3b3d82225a49
victor tran <victor.tran@tempestas.io>
2020-03-26 20:04:45 -0400

    redesign navigation

--- src/components/Form/Navigation.jsx
+++ src/components/Form/Navigation.jsx
@@ -1,39 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { NavLink, NavbarBrand, Navbar, Nav, NavItem } from 'reactstrap';
+// import { Link } from 'react-router-dom';
+// import { NavLink, NavbarBrand, Navbar, Nav, NavItem } from 'reactstrap';
 
-const Navigation = () => {
-    return(
-        <div>
-        <Navbar>
-            <NavbarBrand tag={Link} to="/">
-            AirBnB
-            </NavbarBrand>
-            {localStorage.getItem('token') ? (
-                <Nav className='nav-links'>
-                <NavItem>
-                    <NavLink
-                    tag={Link}
-                    to='/property/new'
-                    >
-                    Add Listing
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink tag={Link} to='#'>
-                    Log Out
-                    </NavLink>
-                </NavItem>
-                </Nav>
-            ) : (
-               <Nav>
-                   <NavItem> <NavLink tag={Link} to="/"> Home </NavLink></NavItem>
-                   <NavItem> <NavLink tag={Link} to="/login"> LogIn </NavLink></NavItem>
-                   <NavItem> <NavLink tag={Link} to="/register"> SignUp</NavLink> </NavItem>
-               </Nav>
-            )}
-        </Navbar>
+const Navigation = ({ className = '' }) => {
+    return (
+        <div className={`${className} nav`.trim()}>
+            <Link className="nav-brand" to="/property">
+                AirBnB Optimal
+            </Link>
+            <div className="nav-sidedrawer button">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            {/* <Navbar>
+                <NavbarBrand tag={Link} to="/">
+                    AirBnB
+                </NavbarBrand>
+                {localStorage.getItem('token') ? (
+                    <Nav className="nav-links">
+                        <NavItem>
+                            <NavLink tag={Link} to="/property/new">
+                                Add Listing
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="#">
+                                Log Out
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                ) : (
+                    <Nav>
+                        <NavItem>
+                            <NavLink tag={Link} to="/">
+                                Home
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/login">
+                                LogIn
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} to="/register">
+                                SignUp
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                )}
+            </Navbar> */}
         </div>
-    )
-}
+    );
+};
 export default Navigation;

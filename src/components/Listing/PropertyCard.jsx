d7969be52f0c8a49d35fe63a8aebcec7b5f25ee9
victor tran <victor.tran@tempestas.io>
2020-03-26 20:06:54 -0400

    show edit button

--- src/components/Listing/PropertyCard.jsx
+++ src/components/Listing/PropertyCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link, useLocation, useHistory } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 
 const PropertyCard = ({
     className = '',
@@ -16,7 +16,7 @@ const PropertyCard = ({
 }) => {
     const location = useLocation();
     const history = useHistory();
-
+    console.log(owner, loggedInAs);
     return (
         <div
             onClick={e => {
@@ -24,7 +24,10 @@ const PropertyCard = ({
                 history.push(`${location.pathname}/${id}`);
             }}
             className={`${className} property card`.trim()}>
-            <img src="https://images.unsplash.com/photo-1559767949-0faa5c7e9992?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+            <img
+                src="https://images.unsplash.com/photo-1559767949-0faa5c7e9992?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+                alt="placeholder"
+            />
             <div className="property-info">
                 <header>
                     <h4>{`${address}, ${city}, ${state.toUpperCase()}, ${zip}`}</h4>
@@ -35,21 +38,17 @@ const PropertyCard = ({
                         <p>Price: {`$${price}`}</p>
                     </div>
                 </section>
-                {// conditionally render only if the owner is looking at them
-                owner === loggedInAs.id && (
-                    <div className="property-controls">
-                        <div
-                            className="edit"
-                            onClick={e => e.stopPropagation()}>
-                            Edit
-                        </div>
-                        <div
-                            className="delete"
-                            onClick={e => e.stopPropagation()}>
-                            Delete
-                        </div>
+                {/* {// conditionally render only if the owner is looking at them */}
+                {/* owner === loggedInAs.id && ( */}
+                <div className="property-controls">
+                    <div className="edit" onClick={e => e.stopPropagation()}>
+                        Edit
                     </div>
-                )}
+                    <div className="delete" onClick={e => e.stopPropagation()}>
+                        Delete
+                    </div>
+                </div>
+                {/* )} */}
             </div>
         </div>
     );

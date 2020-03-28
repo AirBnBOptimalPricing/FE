51b75d8ec0f131f79349a0ddacaf94ec535a5f6e
victor tran <victor.tran@tempestas.io>
2020-03-27 22:22:11 -0400

    updated this to look for token in redux first then localStorage

--- src/components/Route/PrivateRoute.jsx
+++ src/components/Route/PrivateRoute.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { withToken } from '../../util';
+import { connect } from 'react-redux';
 
-const PrivateRoute = ({ component: Component, ...props }) => {
-    const token = withToken();
+const PrivateRoute = ({ component: Component, token, ...props }) => {
+    if (!token) {
+        [token] = withToken();
+    }
     // Needs a safeguard, such as, when a user goes to edit a property, they must OWN that property
     return (
         <Route
@@ -19,4 +22,10 @@ const PrivateRoute = ({ component: Component, ...props }) => {
     );
 };
 
-export default PrivateRoute;
+const mapStateToProps = ({
+    auth: {
+        user: { token },
+    },
+}) => ({ token });
+
+export default connect(mapStateToProps)(PrivateRoute);
